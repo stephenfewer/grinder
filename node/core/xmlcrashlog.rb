ff9574374e45ae5ae857d9ad11fa283db1de67fc
Stephen Fewer <stephen_fewer@harmonysecurity.com>
2012-08-21 15:31:42 +0100

    Add prelim support for log idx's to have parents/children. using the config file you can enable/disable printing of code/message comments.

--- node/core/xmlcrashlog.rb
+++ node/core/xmlcrashlog.rb
@@ -55,28 +55,29 @@ class XmlCrashLog
 			document = ::REXML::Document.new( data )
 			
 		rescue
-			p $!
 			data     = nil
 			document = nil
 		end
 		
 		return false if not data or not document
 
-		document.elements.each do | root |
-		
-			next if( root.name.downcase != 'fuzzer' )
+		parse_log_elements = lambda do | root, level |
+
+			return [] if( root.name.downcase != 'log' )
+
+			clogs = ::Array.new
 			
-			root.each do | element1 |
+			log   = ::Hash.new
+
+			root.each do | child |
 			
-				next if( element1.name.downcase != 'log' )
-				
-				log = ::Hash.new
-				
-				element1.each do | element2 |
-					if( element2.name == 'idx' or element2.name == 'count' )
-						log[element2.name] = element2.text.to_i
-					elsif( element2.name == 'message' )
-						message = element2.text
+				case child.name.downcase
+					when 'log'
+						clogs.concat( parse_log_elements.call( child, level + 1 ) )
+					when 'idx', 'count'
+						log[child.name.downcase] = child.text.to_i
+					when 'message'
+						message = child.text
 						
 						message = message.gsub( '&lt;', '<' )
 						message = message.gsub( '&gt;', '>' )
@@ -84,24 +85,54 @@ class XmlCrashLog
 						message = message.gsub( '&quot;', '"' )
 						message = message.gsub( '&apos;', '\'' )
 						message = message.gsub( '&nbsp;', ' '  )
-			
-						log[element2.name] = message
+				
+						log[child.name.downcase] = message
 					else
-						log[element2.name] = element2.text
-					end
+						log[child.name.downcase] = child.text
 				end
 				
-				@log_lines << log
 			end
+			
+			if( not log.empty? )
+				log['level'] = level
+			end
+			
+			if( not clogs.empty? and log['idx'] )
+				clogs.each do | c |
+					if( not c['parent_idx'] )
+						c['parent_idx']   = log['idx']
+						c['parent_count'] = log['count'] if log['count']
+						
+						#if( not log['last_idx'] or log['last_idx'] < c['idx'] )
+						#	log['last_idx'] = c['idx']
+						#end
+					end
+				end
+			end
+			
+			return [ log ].concat( clogs )
 		end
 		
+		document.elements.each do | root |
+		
+			next if( root.name.downcase != 'fuzzer' )
+			
+			root.each do | child |
+				
+				next if( child.name.downcase != 'log' )
+				
+				@log_lines.concat( parse_log_elements.call( child, 0 ) )
+			end
+
+		end
+
 		@log_lines = @log_lines.sort_by do | log |
 			log['idx']
 		end
 		
 		count_log_ids = lambda do | count |
 			@log_lines.each do | log |
-				if( log['message'].include?( @elem_prefix + count.to_s ) )
+				if( log['message'] and log['message'].include?( @elem_prefix + count.to_s ) )
 					return count_log_ids.call( count + 1 )
 				end
 			end
@@ -114,10 +145,10 @@ class XmlCrashLog
 	end
 
 	# returns an array of log idx's which pass any restrictions (via opts/skip_elem/skip_idx)
-	def generate_idxs( opts={}, skip_elem=[], skip_idx=[] )
+	def generate_idxs( opts={}, skip_elem=[], skip_idx=[], level=nil )
 		idx = []
-		enumerate_log( opts, skip_elem, skip_idx ) do | log |
-			idx << log['idx']
+		enumerate_log( opts, skip_elem, skip_idx, level ) do | log |
+			idx << log['idx'] if log['message']
 		end
 		return idx
 	end
@@ -135,37 +166,53 @@ class XmlCrashLog
 		return elems
 	end
 	
-	def enumerate_log( opts={}, skip_elem=[], skip_idx=[] )
+	def enumerate_log( opts={}, skip_elem=[], skip_idx=[], level=nil )
 		@log_lines.each do | log |
 		
+			if( level and level != log['level'] )
+				next
+			end
+			
 			if( skip_idx.include?( log['idx'] ) )
 				next
 			end
-
+			
 			message = log['message']
-
-			skip = false
 			
-			skip_elem.each do | elem_id |			
-				if( message.index( elem_id ) )
-					skip = true
-					break
+			if( message )
+			
+				skip = false
+				
+				skip_elem.each do | elem_id |			
+					if( message.index( elem_id ) )
+						skip = true
+						break
+					end
 				end
-			end
-			
-			next if skip
-			
-			if( message.start_with?( '/*' ) and message.end_with?( '*/' ) )
-				next if( not opts['print_code_comments'] )
-			end
-			
-			if( message.start_with?( '//' ) )
-				next if not opts['print_message_comments']
+				
+				next if skip
+				
+				if( message.start_with?( '/*' ) and message.end_with?( '*/' ) )
+					next if( not opts['print_code_comments'] )
+				end
+				
+				if( message.start_with?( '//' ) )
+					next if not opts['print_message_comments']
+				end
 			end
 			
 			yield log
 		end
 	end
+	
+	#def find_log( idx )
+	#	@log_lines.each do | log |
+	#		if( log['idx'] and log['idx'] == idx )
+	#			return log
+	#		end
+	#	end
+	#	return nil
+	#end
 	
 	def generate_html( opts={}, skip_elem=[], skip_idx=[] )
 		html  = ''
@@ -200,36 +247,58 @@ class XmlCrashLog
 		end
 
 		html << "\n"
+		#a = []
 		
 		enumerate_log( opts, skip_elem, skip_idx ) do | log |
-		
+				
+			#if( not a.empty? and log['idx'] > a.last )
+			#	html << "\t\t\t\t}\n"
+			#	a.pop
+			#end
+				
 			message = log['message']
 			
-			if( opts['testcase_fixups'] )
-				opts['testcase_fixups'].each do | key, value |
-					message = message.gsub( key, value )
+			if( message )
+			
+				if( opts['testcase_fixups'] )
+					opts['testcase_fixups'].each do | key, value |
+						message = message.gsub( key, value )
+					end
 				end
+				
+				if( opts['uncomment_code_comments'] and message.start_with?( '/*' ) and message.end_with?( '*/' ) )
+					message = message[2, message.length-4]
+				end
+				
+				#if( log['parent_idx'] and not a.include?( log['parent_idx'] ) )
+				#	
+				#	plog = find_log( log['parent_idx'] )
+				#	if( plog['count'] > 1 and not a.include?( log['last_idx'] ) )
+				#		html << "\t\t\t\tfor( var q=0 ; q<#{plog['count']} ; q++ ) { // #{plog['last_idx']}\n"
+				#		a.push( plog['last_idx'] )
+				#	end
+				#end
+				
+				if( log['count'] > 1 )
+					html << "\t\t\t\tfor( var i=0 ; i<#{log['count']} ; i++ ) {\n"
+				end
+				
+				tabs = log['count'] > 1 ? "\t\t\t\t\t" : "\t\t\t\t"
+				
+				#message += " - #{log['parent_idx']}" if log['parent_idx']
+				
+				if( opts['try_catch'] and not message.start_with?( '//' ) )
+					html << "#{tabs}try { #{message} } catch(e){}\n"
+				else
+					html << "#{tabs}#{message}\n"
+				end
+				
+				if( log['count'] > 1 )
+					html << "\t\t\t\t}\n"
+				end
+				
 			end
 			
-			if( opts['uncomment_code_comments'] and message.start_with?( '/*' ) and message.end_with?( '*/' ) )
-				message = message[2, message.length-4]
-			end
-			
-			if( log['count'] > 1 )
-				html << "\t\t\t\tfor( var i=0 ; i<#{log['count']} ; i++ ) {\n"
-			end
-			
-			tabs = log['count'] > 1 ? "\t\t\t\t\t" : "\t\t\t\t"
-			
-			if( opts['try_catch'] and not message.start_with?( '//' ) )
-				html << "#{tabs}try { #{message} } catch(e){}\n"
-			else
-				html << "#{tabs}#{message}\n"
-			end
-			
-			if( log['count'] > 1 )
-				html << "\t\t\t\t}\n"
-			end
 		end
 		
 		html << "\t\t\t" << opts['testcase_append_function'] << "\n" if opts['testcase_append_function']
